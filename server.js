fa0e6cfebfe1e76526435195647e8178a63f9abe
Ruth3r4d1um104 <140322070+Lehlohonolo-Saohatse@users.noreply.github.com>
2025-07-20 22:48:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Schemas
 const categorySchema = new mongoose.Schema({
